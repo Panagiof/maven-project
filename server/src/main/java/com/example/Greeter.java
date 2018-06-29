5ca6312a419c69336a32cfa2063716f3db555bce
Panagiotis Filippou <pr.filippou@gmail.com>
2018-06-29 11:06:21 +0300

    changes 1.0

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -3,24 +3,17 @@ package com.example;
 /**
  * This is a class.
  */
-public class Greeter
-{
+public class Greeter {
 
-    /**
-     * This is a constructor.
-     */
-    public Greeter()
-    {
+  /**
+   * This is a constructor.
+   */
+  public Greeter() {
 
-    }
-    
-    /*
-    * @param someone the name oa a person
-    * @return greeting string
-    */
-    
-    public final String greet(final String someone)
-    {
-        return String.format("Hello, %s!", someone);
-    }
+  }
+
+  //TODO: Add javadoc comment
+  public String greet(String someone) {
+    return String.format("Hello, %s!", someone);
+  }
 }
